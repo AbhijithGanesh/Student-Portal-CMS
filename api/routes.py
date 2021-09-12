cfaf5f7ece6495abeaab7eace788fc79e40bae30
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-12 12:03:25 +0530

    Changes to the API config🚀

--- api/routes.py
+++ api/routes.py
@@ -4,14 +4,17 @@ from pydantic import BaseModel
 from fastapi import APIRouter, Request
 from pydantic.types import Json
 
-router = APIRouter()
 
 class Table(BaseModel):
     Name:str
     Class:str
     Age:int
+    
+student_router = APIRouter()
 
 
-@router.get("/api-endpoint-1")
+
+@student_router.get("/api-endpoint-1")
 def ReturnAll():
-    return {"Key":"Value"}
+    return {"TestData":"Has been returned"}
+
