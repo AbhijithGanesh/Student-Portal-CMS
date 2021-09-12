032ab22f7d634113cd14418572d8edfbb366fc38
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-12 12:02:51 +0530

    Changes to BackEnd Logic

--- CMSLogic/admin.py
+++ CMSLogic/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import StudentProfile, Events, ContactUs
 
-# Register your models here.
+
+admin.site.register([StudentProfile, ContactUs, Events])
--- CMSLogic/models.py
+++ CMSLogic/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from pytz import country_names as c
+from datetime import date
+
+dict_choices = dict(c)
+_choices = []
+_keys = list(dict_choices.keys())
+_value = list(dict_choices.values())
+if len(_keys) == len(_value):
+    for i in range(len(_keys)):
+        a = [_keys[i], _value[i]]
+        _choices.append(tuple(a))
+
+
+class StudentProfile(models.Model):
+    Name = models.CharField(max_length=300)
+    Application_Number = models.BigIntegerField()
+    Date_Of_Birth = models.DateField()
+    Gender = models.CharField(
+        max_length=30,
+        choices=[
+            ("M", "Male"),
+            ("F", "Female"),
+            ("N", "Non-Binary"),
+            ("W", "Would not like to reveal"),
+        ],
+    )
+    HomeState = models.CharField(max_length=300)
+    Country = models.CharField(max_length=75, choices=_choices)
+    ContactNumber = models.BigIntegerField()
+
+class ContactUs(models.Model):
+    Department_Name = models.CharField(max_length=300)
+    Department_Head = models.CharField(max_length=300)
+    Department_ContactDetails = models.IntegerField()
+
+    class Meta:
+       verbose_name_plural = "Contact Us"
+    
+class Events(models.Model):
+    Event_Name = models.CharField(max_length = 50)
+    Event_Head = models.ForeignKey(StudentProfile, on_delete = models.DO_NOTHING)
+    Event_Duration = models.DurationField()
+    Event_Descripton = models.TextField(null = False, default = "Empty Description")
+
+    class Meta:
+       verbose_name_plural = "Events and Notices"
