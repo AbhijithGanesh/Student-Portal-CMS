0e9e6dba8de536d0a5f0745d89b51ce3f0eb6e90
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-29 23:29:29 +0530

    Addition of React Components

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Typography from '@material-ui/core/typography'
+
+const HelloWorld = () => {
+    return (
+        <>
+            <Typography variant = "h1">
+                Hello World
+            </Typography>
+        </>
+    )
+}
+export default HelloWorld;
