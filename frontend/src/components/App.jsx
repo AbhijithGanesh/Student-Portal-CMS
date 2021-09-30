9ac654c370a41816bcf66e80f8c0579a405390f4
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-09-30 20:41:25 +0530

    Addiition of App Bar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Typography from '@material-ui/core/typography'
+import DenseAppBar from './header/header.jsx';
 
 const HelloWorld = () => {
     return (
         <>
+        <DenseAppBar/>
             <Typography variant = "h1">
                 Hello World
             </Typography>
